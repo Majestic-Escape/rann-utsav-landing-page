cbadb2ad6f8b1e9d9aa3b2ff35e2790ca23fb934
Deepam Mac <deepam0511@gmail.com>
2025-10-09 22:54:53 +0530

    gtag scripts

--- index.php
+++ index.php
@@ -15,6 +15,14 @@
         gtag('config', 'G-SW4W868BCN');
     </script>
 
+    <!-- Google Tag Manager -->
+    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-W73276NQ');</script>
+    <!-- End Google Tag Manager -->
+
     <!-- Meta Pixel Code -->
     <script>
         ! function(f, b, e, v, n, t, s) {
@@ -56,6 +64,12 @@
 </head>
 
 <body>
+
+    <!-- Google Tag Manager (noscript) -->
+    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-W73276NQ"
+    height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+    <!-- End Google Tag Manager (noscript) -->
+
     <?php $eventDate = new DateTime('2025-10-23');
     $now = new DateTime();
     $interval = $now->diff($eventDate);
